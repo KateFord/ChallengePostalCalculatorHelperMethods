ab384d4a0c966071843e13178d0e60d725211948
Kate Ford <katefwannell7@gmail.com>
2018-06-22 16:23:57 -0700

    Initial commit

---
+++ ChallengePostalCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:
+        <asp:TextBox ID="widthTextBox" runat="server" OnTextChanged="widthTextBox_TextChanged" AutoPostBack="true"></asp:TextBox>
+        <br />
+        Height:
+        <asp:TextBox ID="heightTextBox" runat="server" OnTextChanged="heightTextBox_TextChanged" AutoPostBack="true"></asp:TextBox>
+        <br />
+        Length: <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" GroupName="shipping" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" GroupName="shipping" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+               ViewState.Add("MyValue", "");
+            }
+            
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string value = ViewState["MyValue"].ToString();
+            value = widthTextBox.Text;
+            ViewState["MyValue"] = value;
+            resultLabel.Text = "in width event ..." + value;
+           
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = "in ground event";
+        }
+                
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = "in air event";
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            resultLabel.Text = "length event";
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string value = ViewState["MyValue"].ToString();
+            value = heightTextBox.Text;
+            ViewState["MyValue"] = value;
+            resultLabel.Text = "in height event ..." + value;
+        }
+    }
+}
